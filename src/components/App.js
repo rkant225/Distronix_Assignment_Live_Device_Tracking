638aafa26c27e987a9ce61f8f8777caf7df5a4b2
Rahul Singh <rkant225@gmail.com>
2020-05-28 02:53:06 +0530

    converted BrowserRouter to HashRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {HashRouter, Route} from 'react-router-dom';
 
 import Home from './Home/Home';
 import Map from './Map/Map';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="ui container">
-        <Route path="Distronix_Assignment_Live_Device_Tracking/" exact render={(props)=><Home {...props}/>}/>
-        <Route path="Distronix_Assignment_Live_Device_Tracking/map/:deviceId/:sensorId" exact render={(props)=><Map {...props}/>}/>
+        <Route path="/" exact render={(props)=><Home {...props}/>}/>
+        <Route path="/map/:deviceId/:sensorId" exact render={(props)=><Map {...props}/>}/>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
