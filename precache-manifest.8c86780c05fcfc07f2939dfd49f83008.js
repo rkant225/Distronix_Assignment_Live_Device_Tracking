6d551685be8b9d5bf32490a91bed7233e35b13b5
Rahul Singh <rkant225@gmail.com>
2020-05-28 02:43:10 +0530

    Updates

---
+++ precache-manifest.8c86780c05fcfc07f2939dfd49f83008.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9602999c043529182bc2fe6ce371020e",
+    "url": "/Distronix_Assignment_Live_Device_Tracking/index.html"
+  },
+  {
+    "revision": "6db21d4b63e03153a8c8",
+    "url": "/Distronix_Assignment_Live_Device_Tracking/static/css/main.ed57398d.chunk.css"
+  },
+  {
+    "revision": "d2a7102fe7ffedab842f",
+    "url": "/Distronix_Assignment_Live_Device_Tracking/static/js/2.d140b169.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/Distronix_Assignment_Live_Device_Tracking/static/js/2.d140b169.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6db21d4b63e03153a8c8",
+    "url": "/Distronix_Assignment_Live_Device_Tracking/static/js/main.41a5faca.chunk.js"
+  },
+  {
+    "revision": "fe047fe11c664033c4e4",
+    "url": "/Distronix_Assignment_Live_Device_Tracking/static/js/runtime-main.1967b2de.js"
+  }
+]);
